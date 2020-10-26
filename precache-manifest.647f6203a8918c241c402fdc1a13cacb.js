ca8ce45a3b05c19caf18a813cdf00393b6bb3218
Ps521520 <ps521520@gmail.com>
2020-10-26 13:17:59 +0530

    Updates

---
+++ precache-manifest.647f6203a8918c241c402fdc1a13cacb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b3ca9abb6cfd3df11b4627c45ab7ff",
+    "url": "/moviesearch/index.html"
+  },
+  {
+    "revision": "9471b872297ee391bae9",
+    "url": "/moviesearch/static/css/main.0e8c6c6e.chunk.css"
+  },
+  {
+    "revision": "c37f4bcdd14c6381ab4a",
+    "url": "/moviesearch/static/js/2.2e0bddb9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/moviesearch/static/js/2.2e0bddb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9471b872297ee391bae9",
+    "url": "/moviesearch/static/js/main.bd20484c.chunk.js"
+  },
+  {
+    "revision": "b5a767c26bfb1de4a3b5",
+    "url": "/moviesearch/static/js/runtime-main.aa432347.js"
+  }
+]);
